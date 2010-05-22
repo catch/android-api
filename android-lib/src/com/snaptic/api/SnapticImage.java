432e15c03d2770e055dd0510beb4b37cae687157
htormey <harry@p2presearch.com>
2010-05-22 16:06:57 -0700

    readding files that got nuked fromt git

---
+++ android-lib/src/com/snaptic/api/SnapticImage.java
@@ -0,0 +1,58 @@
+package com.snaptic.api;
+/*
+ * Copyright (c) 2010 Snaptic, Inc
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * http://snaptic.com Java Android library
+ * 
+ * Hugh Johnson   <hugh@snaptic.com>
+ * Harry Tormey   <harry@snaptic.com>
+ */
+
+/**
+ * @author Hugh Johnson
+ * @author Harry Tormey
+ * @version 0.1
+ * 
+ * Data structure that represents used to represent an image.
+ */
+public class SnapticImage {
+	public int width;
+	public int height;
+	public String src;
+	public long id;
+	public int order;
+	public String md5;
+	public long revision_id;
+	
+	public SnapticImage() {
+		width = 0;
+		height = 0;
+		src = null;
+		id = -1;
+		order = -1;
+		md5 = null;
+		revision_id = -1;
+	}
+	
+	public SnapticImage(long id, String src, int width, int height, int order, String md5, long revision_id) {
+		this.width = width;
+		this.height = height;
+		this.src = src;
+		this.id = id;
+		this.order = order;
+		this.md5 = md5;
+		this.revision_id = revision_id;
+	}	
+}
